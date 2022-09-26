55e9c7e0939bbe25e0618cd24584af131a4d24b6
TrueCebola <carloseduardotdes@gmail.com>
2022-09-26 13:55:28 -0300

    Modified files

--- cabecalho.php
+++ cabecalho.php
@@ -45,6 +45,9 @@
         <li class="nav-item">
             <a class="nav-link" href="./clientes.php">Clientes</a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="./disciplinas.php">Disciplinas</a>
+        </li>
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
--- disciplinas.php
+++ disciplinas.php
@@ -2,8 +2,48 @@
 
 <h1>Disciplinas</h1>
 
+<?php
+    //Traz conteudo da url
+    $dados = file_get_contents("https://reserva.fatectq.edu.br/api/disciplinas");
+    //Transforma JSON em array associativo
+    $dados = json_decode($dados,true);
+    // echo "<pre>";
+    // print_r($dados);
+    // echo "</pre>";
+?>
+
 <div class="row">
     <div class="col-10 offset-1">
+        <table class="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Apelido</th>
+                    <th>Sigla</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    for($i=0;$i<count($dados);$i++){
+                ?>
+            <tr>
+                <td><?php echo $dados[$i]["disciplinaId"]; ?></td>
+                <td><?php echo $dados[$i]["nome"]; ?></td>
+                <td><?php echo $dados[$i]["apelido"]; ?></td>
+                <td><?php echo $dados[$i]["sigla"]; ?></td>
+                <td>
+                    <a href="" class="btn btn-warning">
+                        Editar
+                    </a>
+                </td>
+            </tr>
+            <?php
+                }
+            ?>
+            </tbody>
+        </table>
     </div>
 </div>
 
