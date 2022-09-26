7aaad985c371047de06c475b334d9f96c30a864a
TrueCebola <carloseduardotdes@gmail.com>
2022-09-26 15:52:20 -0300

    footer

--- rodape.php
+++ rodape.php
@@ -1,5 +1,11 @@
 </div>
-<footer>Desenvolvido por Carlos Torrichelli</footer>
+<footer>Desenvolvido por Carlos Torrichelli 
+        <script>
+            var today = new Date();
+            var year = today.getFullYear();
+            document.write(year)
+        </script>
+</footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
 
     </script>
